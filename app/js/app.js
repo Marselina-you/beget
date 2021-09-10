ffdbe7bd6600b6e54e7bb85a0efb83c0b3210232
unknown <marselina88@list.ru>
2021-09-10 18:20:52 +0300

    10.09

--- app/js/app.js
+++ app/js/app.js
@@ -41,3 +41,59 @@ var main = function() { //главная функция
 
 $(document).ready(main); /* как только страница полностью загрузится, будет
                вызвана функция main, отвечающая за работу меню */
+ $(document).ready(function() {
+     function htmSlider(){
+        /* Зададим следующие переменные */
+
+        /* обертка слайдера */
+        var slideWrap = $('.slide-wrap');
+        /* ссылки на предудыщий иследующий слайд */
+        var nextLink = $('.next-slide');
+        var prevLink = $('.prev-slide');
+        var is_animate = false;
+        /* ширина слайда с отступами */
+        var slideWidth = $('.slide-item').outerWidth();
+        /* смещение слайдера */
+        var newLeftPos = slideWrap.position().left - slideWidth;
+        /* Клик по ссылке на следующий слайд */
+        nextLink.click(function(){
+            if(!slideWrap.is(':animated')) {
+                slideWrap.animate({left: newLeftPos}, 500, function(){
+                    slideWrap
+                        .find('.slide-item:first')
+                        .appendTo(slideWrap)
+                        .parent()
+                        .css({'left': 0});
+                    });
+            }
+        });
+/* Клик по ссылке на предыдующий слайд */
+        prevLink.click(function(){
+            if(!slideWrap.is(':animated')) {
+                slideWrap
+                    .css({'left': newLeftPos})
+                    .find('.slide-item:last')
+                    .prependTo(slideWrap)
+                    .parent()
+                    .animate({left: 0}, 500);
+                }
+        });
+        /* Клики по ссылкам старт/пауза */
+        playLink.click(function(){
+            if(playLink.hasClass('play')){
+                playLink.removeClass('play').addClass('pause');
+                jQuery('.navy').addClass('disable');
+                timer = setInterval(autoplay, 1000);
+            } else {
+                playLink.removeClass('pause').addClass('play');
+                jQuery('.navy').removeClass('disable');
+                clearInterval(timer);
+            }
+        });
+
+    }
+/* иницилизируем функцию слайдера */
+    htmSlider();
+
+
+});              
